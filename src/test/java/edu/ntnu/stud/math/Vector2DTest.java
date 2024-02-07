9da0723c6558a325e0e52e886388f97a82d17463
Josefine Arntsen <josefia@ulrik.uio.no>
2024-02-07 14:05:17 +0100

    Feat: Initialized test class for Vector2D

---
+++ src/test/java/edu/ntnu/stud/math/Vector2DTest.java
@@ -0,0 +1,26 @@
+package edu.ntnu.stud.math;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class Vector2DTest {
+
+  @org.junit.jupiter.api.BeforeEach
+  void setUp() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getX0() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void getX1() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void add() {
+  }
+
+  @org.junit.jupiter.api.Test
+  void subtract() {
+  }
+}
