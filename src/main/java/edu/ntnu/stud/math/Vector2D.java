fca0235d50e37088a7bd4f8a1c3a5315800a48ea
Josefine Arntsen <josefia@ulrik.uio.no>
2024-02-07 14:23:50 +0100

    Docs: Add javadoc

--- src/main/java/edu/ntnu/stud/math/Vector2D.java
+++ src/main/java/edu/ntnu/stud/math/Vector2D.java
@@ -1,13 +1,17 @@
 package edu.ntnu.stud.math;
 
+/**
+ * Represents a two-dimensional vector with double precision floating-point components.
+ */
 public class Vector2D {
   private final double x0;
   private final double x1;
 
   /**
-   * Constructor for Vector2D
-   * @param x0 - The first element in the vector.
-   * @param x1 - The second element in the vector.
+   * Constructs a Vector2D with the specified components.
+   *
+   * @param x0 The first element of the vector.
+   * @param x1 The second element of the vector.
    */
   public Vector2D(double x0, double x1) {
     this.x0 = x0;
@@ -15,27 +19,41 @@ public class Vector2D {
   }
 
   /**
-   * Getter for x0.
-   * @return x0.
+   * Retrieves the value of the first component (x0) of the vector.
+   *
+   * @return The value of the first component.
    */
   public double getX0() {
     return x0;
   }
 
   /**
-   * Getter for x1.
-   * @return x1.
+   * Retrieves the value of the second component (x1) of the vector.
+   *
+   * @return The value of the second component.
    */
   public double getX1() {
     return x1;
   }
 
+  /**
+   * Adds another Vector2D to this vector.
+   *
+   * @param other The vector to be added.
+   * @return A new Vector2D representing the result of the addition.
+   */
   public Vector2D add(Vector2D other) {
     double first_element = this.getX0() + other.getX0();
     double second_element = this.getX1() + other.getX1();
     return new Vector2D(first_element, second_element);
   }
 
+  /**
+   * Subtracts another Vector2D from this vector.
+   *
+   * @param other The vector to be subtracted.
+   * @return A new Vector2D representing the result of the subtraction.
+   */
   public Vector2D subtract(Vector2D other) {
     double first_element = this.getX0() - other.getX0();
     double second_element = this.getX1() - other.getX1();
