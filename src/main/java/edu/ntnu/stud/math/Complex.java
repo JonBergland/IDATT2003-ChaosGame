ee47dda6cf473a45f4c7d902c4385304155ff07c
Jon Bergland <jobergl@stud.ntnu.no>
2024-02-07 14:01:38 +0100

    Feat: Override methods from Vector and add sqrt-method

--- src/main/java/edu/ntnu/stud/math/Complex.java
+++ src/main/java/edu/ntnu/stud/math/Complex.java
@@ -2,6 +2,25 @@ package edu.ntnu.stud.math;
 
 public class Complex extends Vector2D {
   public Complex(double realPart, double imaginaryPart){
-    //super(realPart, imaginaryPart);
+    super(realPart, imaginaryPart);
+  }
+
+  @Override
+  public Vector2D add(Vector2D other) {
+    return super.add(other);
+  }
+
+  @Override
+  public Vector2D subtract(Vector2D other) {
+    return super.subtract(other);
+  }
+
+
+  /**
+   *
+   * @return Complex vector
+   */
+  public Complex sqrt(){
+    return new Complex(0,0);
   }
 }
