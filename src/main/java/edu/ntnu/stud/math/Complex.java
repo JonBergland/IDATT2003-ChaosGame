21ca2cd08f8221641068af8a3d707e0dc02fc1cf
Jon Bergland <jobergl@stud.ntnu.no>
2024-02-07 13:55:53 +0100

    Feat: Add constructor

--- src/main/java/edu/ntnu/stud/math/Complex.java
+++ src/main/java/edu/ntnu/stud/math/Complex.java
@@ -1,4 +1,7 @@
 package edu.ntnu.stud.math;
 
-public class Complex {
+public class Complex extends Vector2D {
+  public Complex(double realPart, double imaginaryPart){
+    //super(realPart, imaginaryPart);
+  }
 }
